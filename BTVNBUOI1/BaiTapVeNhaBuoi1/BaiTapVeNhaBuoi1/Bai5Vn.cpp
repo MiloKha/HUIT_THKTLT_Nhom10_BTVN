a4b259cb07810178b73151c94e475c49bbc05066
NguyenHuuChi <huuchitn@gmail.com>
2024-08-04 16:35:58 +0700

    BTVNB1_Bai5

---
+++ BTVNBUOI1/BaiTapVeNhaBuoi1/BaiTapVeNhaBuoi1/Bai5Vn.cpp
@@ -0,0 +1,26 @@
+﻿#include <stdio.h>
+
+int main() {
+    int a, b, c;
+    float result;
+
+    // Nhập 3 số nguyên a, b, c
+    printf("Nhập số nguyên a: ");
+    scanf("%d", &a);
+    printf("Nhập số nguyên b: ");
+    scanf("%d", &b);
+    printf("Nhập số nguyên c: ");
+    scanf("%d", &c);
+
+    // Kiểm tra nếu a-b bằng 0 để tránh chia cho 0
+    if (a - b == 0) {
+        printf("Lỗi: Phép tính không hợp lệ vì a - b = 0, không thể thực hiện phép chia.\n");
+    }
+    else {
+        // Thực hiện phép chia c / (a - b)
+        result = (float)c / (a - b);
+        printf("Kết quả của c / (a - b) là: %.2f\n", result);
+    }
+
+    return 0;
+}
