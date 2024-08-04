84369b783b9170820e444703d1cab6e15ebc21bd
NguyenHuuChi <huuchitn@gmail.com>
2024-08-04 17:19:16 +0700

    BTVNB1_Bai11_Caub

--- BTVNBUOI1/BaiTapVeNhaBuoi1/BaiTapVeNhaBuoi1/Bai11.cpp
+++ BTVNBUOI1/BaiTapVeNhaBuoi1/BaiTapVeNhaBuoi1/Bai11.cpp
@@ -18,7 +18,18 @@ void printLocalMinima(int arr[], int size) {
     }
     printf("\n");
 }
+// Hàm xóa phần tử tại vị trí k
+void deleteElement(int arr[], int* size, int k) {
+    if (k < 0 || k >= *size) {
+        printf("Vi tri xoa khong hop le.\n");
+        return;
+    }
 
+    for (int i = k; i < *size - 1; i++) {
+        arr[i] = arr[i + 1];
+    }
+    (*size)--;
+}
 
 int main() {
     int arr[SIZE] = { 1, 2, 4, 3, 7, 8, 6, 9, 10, 5 };
@@ -29,7 +40,7 @@ int main() {
         // Hiển thị menu
         printf("\nMenu:\n");
         printf("1. Xuat cac so cuc tieu\n");
-
+        printf("2. Xoa phan tu tai vi tri k\n");
         printf("6. Thoat\n");
         printf("Chon tu 1 den 6: ");
         scanf("%d", &choice);
@@ -41,7 +52,16 @@ int main() {
         case 1:
             printLocalMinima(arr, size);
             break;
-       
+        case 2:
+            printf("Nhap vi tri k: ");
+            scanf("%d", &k);
+            deleteElement(arr, &size, k);
+            printf("Mang sau khi xoa phan tu: ");
+            for (int i = 0; i < size; i++) {
+                printf("%d ", arr[i]);
+            }
+            printf("\n");
+            break;
         case 6:
             printf("Thoat chuong trinh.\n");
             break;
