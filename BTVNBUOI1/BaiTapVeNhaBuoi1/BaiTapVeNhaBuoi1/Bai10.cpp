c96aca9499799aeb141880f4c475146d0f39ed8b
NguyenHuuChi <huuchitn@gmail.com>
2024-08-04 17:03:12 +0700

    BTVNB1_Bai10_Caub

--- BTVNBUOI1/BaiTapVeNhaBuoi1/BaiTapVeNhaBuoi1/Bai10.cpp
+++ BTVNBUOI1/BaiTapVeNhaBuoi1/BaiTapVeNhaBuoi1/Bai10.cpp
@@ -3,12 +3,20 @@
 #include <string.h>
 
 // Hàm xuất giá trị từng ký tự của chuỗi thông qua con trỏ
-void printCharacters(char* st) {
+void printCharacters(char *st) {
     while (*st) {
         printf("%c ", *st);
         st++;
     }
     printf("\n");
+}
+
+// Hàm chuyển các ký tự của chuỗi về dạng chữ hoa
+void convertToUpper(char *st) {
+    while (*st) {
+        *st = toupper((unsigned char)*st);
+        st++;
+    }
 }
 
 
@@ -20,18 +28,19 @@ int main() {
     // Nhập chuỗi
     printf("Nhap chuoi: ");
     fgets(st, sizeof(st), stdin);
-
+    
     // Xóa ký tự newline nếu có
     size_t len = strlen(st);
-    if (len > 0 && st[len - 1] == '\n') {
-        st[len - 1] = '\0';
+    if (len > 0 && st[len-1] == '\n') {
+        st[len-1] = '\0';
     }
 
     do {
         // Hiển thị menu
         printf("\nMenu:\n");
         printf("1. Xuat gia tri tung ky tu\n");
-        
+        printf("2. Chuyen cac ky tu ve dang chu hoa\n");
+        printf("3. Chuyen ky tu dau moi tu ve dang chu hoa\n");
         printf("4. Thoat\n");
         printf("Chon tu 1 den 4: ");
         scanf("%d", &choice);
@@ -40,16 +49,20 @@ int main() {
         while (getchar() != '\n');
 
         switch (choice) {
-        case 1:
-            printCharacters(st);
-            break;
-        
-        case 4:
-            printf("Thoat chuong trinh.\n");
-            break;
-        default:
-            printf("Lua chon khong hop le. Vui long chon lai.\n");
-            break;
+            case 1:
+                printCharacters(st);
+                break;
+            case 2:
+                convertToUpper(st);
+                printf("Chuoi sau khi chuyen ve chu hoa: %s\n", st);
+                break;
+          
+            case 4:
+                printf("Thoat chuong trinh.\n");
+                break;
+            default:
+                printf("Lua chon khong hop le. Vui long chon lai.\n");
+                break;
         }
     } while (choice != 4);
 
