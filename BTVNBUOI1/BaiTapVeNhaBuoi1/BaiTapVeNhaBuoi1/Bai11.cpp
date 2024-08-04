4f82fc42f37853488e147df6d3ee9818c8502608
NguyenHuuChi <huuchitn@gmail.com>
2024-08-04 17:12:43 +0700

    BTVNB1_Bai11_Caua

---
+++ BTVNBUOI1/BaiTapVeNhaBuoi1/BaiTapVeNhaBuoi1/Bai11.cpp
@@ -0,0 +1,55 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE 10
+
+// Hàm xuất các số cực tiểu trong mảng
+void printLocalMinima(int arr[], int size) {
+    if (size < 3) {
+        printf("Mang phai co it nhat 3 phan tu.\n");
+        return;
+    }
+
+    printf("Cac so cuc tieu trong mang: ");
+    for (int i = 1; i < size - 1; i++) {
+        if (arr[i] < arr[i - 1] && arr[i] < arr[i + 1]) {
+            printf("%d ", arr[i]);
+        }
+    }
+    printf("\n");
+}
+
+
+int main() {
+    int arr[SIZE] = { 1, 2, 4, 3, 7, 8, 6, 9, 10, 5 };
+    int size = 10;
+    int choice, k, x;
+
+    do {
+        // Hiển thị menu
+        printf("\nMenu:\n");
+        printf("1. Xuat cac so cuc tieu\n");
+
+        printf("6. Thoat\n");
+        printf("Chon tu 1 den 6: ");
+        scanf("%d", &choice);
+
+        // Xóa ký tự newline sau khi nhập số
+        while (getchar() != '\n');
+
+        switch (choice) {
+        case 1:
+            printLocalMinima(arr, size);
+            break;
+       
+        case 6:
+            printf("Thoat chuong trinh.\n");
+            break;
+        default:
+            printf("Lua chon khong hop le. Vui long chon lai.\n");
+            break;
+        }
+    } while (choice != 6);
+
+    return 0;
+}
