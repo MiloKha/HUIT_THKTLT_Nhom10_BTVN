78194c77c91dc68226394e6e12418369a8071a9f
NguyenHuuChi <huuchitn@gmail.com>
2024-08-06 13:51:34 +0700

    BTVNBuoi5_Bai9_Cau2

--- BTVNBUOI5/BTVNBuoi5/BTVNBuoi5/Bai9.cpp
+++ BTVNBUOI5/BTVNBuoi5/BTVNBuoi5/Bai9.cpp
@@ -15,7 +15,20 @@ double S1_iterative(int n) {
     }
     return sum;
 }
+// Hàm tính S2(n) theo cách đệ quy
+double S2_recursive(int n) {
+    if (n == 1) return 1.0;
+    return pow(n, 2) + S2_recursive(n - 1);
+}
 
+// Hàm tính S2(n) theo cách khử đệ quy
+double S2_iterative(int n) {
+    double sum = 0.0;
+    for (int i = 1; i <= n; ++i) {
+        sum += pow(i, 2);
+    }
+    return sum;
+}
 
 
 int main() {
@@ -25,6 +38,8 @@ int main() {
         printf("Menu:\n");
         printf("1. Tinh S1(n) theo cach de quy\n");
         printf("2. Tinh S1(n) theo cach khu de quy\n");
+        printf("3. Tinh S2(n) theo cach de quy\n");
+        printf("4. Tinh S2(n) theo cach khu de quy\n");
         printf("9. Thoat\n");
         printf("Nhap lua chon cua ban: ");
         scanf("%d", &choice);
@@ -41,7 +56,12 @@ int main() {
         case 2:
             printf("S1(%d) theo cach khu de quy = %.6f\n", n, S1_iterative(n));
             break;
-
+        case 3:
+            printf("S2(%d) theo cach de quy = %.6f\n", n, S2_recursive(n));
+            break;
+        case 4:
+            printf("S2(%d) theo cach khu de quy = %.6f\n", n, S2_iterative(n));
+            break;
         case 9:
             printf("Thoat chuong trinh.\n");
             break;
