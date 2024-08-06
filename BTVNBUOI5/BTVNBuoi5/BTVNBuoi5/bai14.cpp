5cc382a940932e8dde0b591de5fd0bc4e6ce689e
NguyenHuuChi <huuchitn@gmail.com>
2024-08-06 14:57:16 +0700

    BTVNBuoi5_Bai14

---
+++ BTVNBUOI5/BTVNBuoi5/BTVNBuoi5/bai14.cpp
@@ -0,0 +1,25 @@
+﻿#include <stdio.h>
+
+// Hàm tính U(n) đệ quy
+int U_recursive(int n) {
+    if (n < 6) {
+        return n;
+    }
+    else {
+        return U_recursive(n - 5) + U_recursive(n - 4) + U_recursive(n - 3) + U_recursive(n - 2) + U_recursive(n - 1);
+    }
+}
+
+int main() {
+    int n;
+
+    // Nhập giá trị n từ bàn phím
+    printf("Nhap n: ");
+    scanf("%d", &n);
+
+    // Tính giá trị của U(n)
+    int result = U_recursive(n);
+    printf("U(%d) = %d\n", n, result);
+
+    return 0;
+}
